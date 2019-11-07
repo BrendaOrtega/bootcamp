e4f3ec6a0ca81fe1ec1a5cad66cf4d2d4351ee74
Héctor BlisS <contacto@fixter.org>
2019-11-06 18:41:27 -0600

    bootcamp card dates

--- src/components/bootcamp/BootcampCard.js
+++ src/components/bootcamp/BootcampCard.js
@@ -6,7 +6,7 @@ import { withRouter } from 'react-router-dom'
 
 
 
-function BootcampCard({ history, weeks, bootcamp, title, students = 100, _id, thumbnail }) {
+function BootcampCard({ startDate, endDate, history, weeks, bootcamp, title, students = 100, _id, thumbnail }) {
     let available = false
     let week1 = false
     if (weeks && weeks[0] && (moment(weeks[0].startDate) < moment(Date.now()))) {
@@ -28,9 +28,9 @@ function BootcampCard({ history, weeks, bootcamp, title, students = 100, _id, th
                 <h3>Bootcamp Online: {title}</h3>
                 <p>Crea las bases que necesitas para convertirte en desarrollador web y comenzar tu carrera profesional. ⚛️</p>
                 <p> <FontAwesome name="user" /> {students.length + 152} Estudiantes</p>
-                <p> <FontAwesome name="calendar" /> Inicia: <strong> 21 Octubre 2019</strong></p>
-                <p> <FontAwesome name="calendar" /> Prework Disponible:  {available && <strong style={{ color: "green" }}>{moment(weeks[0].startDate).format('ll')}</strong> || <strong> 23 Septiembre 2019</strong>}</p>
-                <p> <FontAwesome name="file" /> Exámen final: <b>11 Noviembre 2019 </b> </p>
+                <p> <FontAwesome name="calendar" /> Inicia: <strong> {moment(startDate).format("ll")}</strong></p>
+                <p> <FontAwesome name="calendar" /> Prework Disponible:  {available && <strong style={{ color: "green" }}>{moment(weeks[0].startDate).format('ll')}</strong> || <strong style={{ color: "green" }}> 11 de Noviembre, 2019</strong>}</p>
+                <p> <FontAwesome name="file" /> Exámen final: <b>{endDate ? moment(endDate).format('ll') : "23 Noviembre 2019"} </b> </p>
                 <button className="btn-in">
                     {week1 ? <span
                         onClick={() => history.push("/bootcamp/detail/" + _id)}
