905920a50734b9c282db81356c95732f73049cbc
Fixter <fixter@MacBook-Pro-de-Fixter.local>
2020-02-03 23:45:37 -0600

    exam V1.0 ready2

--- src/components/bootcamp/BootcampCard.js
+++ src/components/bootcamp/BootcampCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FontAwesome from "react-fontawesome"
 import js from "../../assets/JavaScript-logo.png"
 import moment from 'moment'
@@ -8,18 +8,25 @@ import img from "../../assets/28698604_1973144766082686_589124668727305128_o.jpg
 
 
 function BootcampCard({ startDate, endDate, history, weeks, exam, bootcamp, title, students = 100, _id, thumbnail }) {
+    let [examAvailable, setExamAvailable] = useState(false)
     let available = false
     let week1 = false
-    let examAvailable = false
+
+
+    useEffect(() => {
+        console.log(exam)
+        if (exam && (moment(exam[0].startDate) < moment(Date.now()))) {
+            setExamAvailable(true)
+        }
+    }, [exam])
+
     if (weeks && weeks[0] && (moment(weeks[0].startDate) < moment(Date.now()))) {
         available = true
     }
     if (weeks && weeks[1] && (moment(weeks[1].startDate) < moment(Date.now()))) {
         week1 = true
     }
-    if (exam && exam[0] && (moment(exam[0].startDate) < moment(Date.now()))) {
-        examAvailable = true
-    }
+
     let link = "https://firebasestorage.googleapis.com/v0/b/fixter-67253.appspot.com/o/assets%2Fphoto.jpg?alt=media&token=cbbffbc4-d321-4e8c-bc20-abcc83bc8710"
     return (
 
@@ -34,9 +41,9 @@ function BootcampCard({ startDate, endDate, history, weeks, exam, bootcamp, titl
                 <h3>Bootcamp Online: {title}</h3>
                 <p>Crea las bases que necesitas para convertirte en desarrollador web y comenzar tu carrera profesional. ⚛️</p>
                 <p> <FontAwesome name="user" /> {students.length + 195} Estudiantes</p>
-                <p> <FontAwesome name="calendar" /> Inicia: <strong> 21 Octubre 2019</strong></p>
-                <p> <FontAwesome name="calendar" /> Prework Disponible:  {available && <strong style={{ color: "#689f63" }}>{moment(weeks[0].startDate).format('ll')}</strong> || <strong> 23 Septiembre 2019</strong>}</p>
-                <p> <FontAwesome name="file" /> Exámen final: <b>11 Noviembre 2019 </b> </p>
+                <p> <FontAwesome name="calendar" /> Inicia: <strong> 21 Febrero 2020</strong></p>
+                <p> <FontAwesome name="calendar" /> Prework Disponible:  {available && <strong style={{ color: "#689f63" }}>{moment(weeks[0].startDate).format('ll')}</strong> || <strong> 10 Febrero 2020</strong>}</p>
+                <p> <FontAwesome name="file" /> Exámen final: <b>11 Marzo 2020 </b> </p>
                 <button className="btn-in">
                     {week1 ? <span
                         onClick={() => history.push("/bootcamp/detail/" + _id)}
@@ -45,7 +52,7 @@ function BootcampCard({ startDate, endDate, history, weeks, exam, bootcamp, titl
                 {available && <button
                     onClick={() => history.push("/bootcamp/detail/" + _id)}
                     className="btn-pre">Prework ya Disponible</button>}
-                {(exam && exam.length && examAvailable) && <button
+                {examAvailable && <button
                     onClick={() => history.push("/exam/" + _id)}
                     className="btn-pre-exam">Examen ya Disponible</button>}
             </div>
